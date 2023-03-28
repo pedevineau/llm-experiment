832462684bd0833f4013c0fdeaa3c32b58a241e1
pedevineau <pedevineau@hotmail.fr>
2023-03-28 09:51:14 +0200

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -4,6 +4,6 @@ TARGET_FOLDER=${WORK_DIR}/llm-experiment
 MODEL_SIZE=7B
 LLAMA_FOLDER=${TARGET_FOLDER}/llama
 MP=1
-cd $LLAMA_FOLDER & pip install -e
+cd $LLAMA_FOLDER & pip install -e .
 
 torchrun --nproc_per_node $MP ${TARGET_FOLDER}/example.py --ckpt_dir ${WORK_DIR}/${MODEL_SIZE} --tokenizer_path ${WORK_DIR}/tokenizer.model
